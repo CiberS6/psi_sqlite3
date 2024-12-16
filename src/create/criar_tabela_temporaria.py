bacd03d0ffc78b3966189320a50cfd07a1d891ce
CiberS6 <120577674+CiberS6@users.noreply.github.com>
2024-12-16 10:15:22 +0000

    Update criar_tabela_temporaria.py

--- src/create/criar_tabela_temporaria.py
+++ src/create/criar_tabela_temporaria.py
@@ -1,15 +1,24 @@
 import sqlite3
 
 # Estabelecer conexão com a base de dados SQLite
-connection = sqlite3.connect('C:\\Users\\ba2490\\Documents\\GitHub\\psi_sqlite3\\sqlite_database\\lol.db')
+connection = sqlite3.connect('C:\\Users\\ba2490\\Desktop\\psi_sqlite3_new\\psi_sqlite3\\sqlite_database\\lol.db')
 cursor = connection.cursor()
 
 cursor.execute('''
-               CREATE TEMPORARY TABLE champions_temp AS,
-               SELECT nome_champion, id_role, id_build,
-               FROM champions,
+               CREATE TEMPORARY TABLE champions_temp AS
+               SELECT nome_champion, id_role, id_build
+               FROM champions
                ORDER BY nome_champion
                 ''')
 
+cursor.execute('DELETE FROM champions')
+
+cursor.execute(''' 
+                INSERT INTO champions (id_champion, nome_champion, id_role, id_build)
+                SELECT NULL, nome_champion, id_role, id_build
+                FROM champions_temp;    
+''')
+
+cursor.execute('DROP TABLE champions_temp')
 cursor.execute('')
 
